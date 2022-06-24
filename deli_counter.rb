1e97b684c343051b1a3c2fbc3cb8762d3cd0c070
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-24 12:54:58 +0300

    add solution

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def line(katz_deli)
+    if katz_deli.count == 0
+        return puts "The line is currently empty."
+    end
+    queue = "The line is currently:"
+    katz_deli.each.with_index(1){|customer, index| queue << " #{index}. #{customer}"}
+    puts queue
+end
+
+def take_a_number(katz_deli,name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        return puts "There is nobody waiting to be served!"
+    end
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+end
